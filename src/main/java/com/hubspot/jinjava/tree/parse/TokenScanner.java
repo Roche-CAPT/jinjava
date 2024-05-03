3589a62b6e9954ae8c83839fba5ae029c1d76e93
Greg Gibeling <greg.gibeling@roche.com>
2024-05-02 19:07:32 -0700

    Support the trim character on endraw tags

--- src/main/java/com/hubspot/jinjava/tree/parse/TokenScanner.java
+++ src/main/java/com/hubspot/jinjava/tree/parse/TokenScanner.java
@@ -209,6 +209,11 @@ public class TokenScanner extends AbstractIterator<Token> {
 
   private boolean isEndRaw() {
     int pos = currPost + 1;
+    if (pos < length) {
+    	if (is[pos] == symbols.getTrimChar()) {
+    		pos++;
+    	}
+    }
     while (pos < length) {
       if (!Character.isWhitespace(is[pos++])) {
         break;
